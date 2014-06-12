79d708e2090216a6b8e9f999877dcacbb9ee90b1
Aris <ak.koliopoulos@gmail.com>
2014-06-12 09:45:46 -0700

    a wekaSparkUtils class that includes various utilities needed in the main package

---
+++ Sparking/src/main/java/uk/ac/manchester/ariskk/distributedWekaSpark/main/wekaSparkUtils.scala
@@ -0,0 +1,33 @@
+package uk.ac.manchester.ariskk.distributedWekaSpark.main
+
+import java.util.ArrayList
+import weka.core.Instances
+import java.io.BufferedReader
+import weka.classifiers.Classifier
+import weka.clusterers.Clusterer
+
+/**Contains various utilities
+ * 
+ * @author Aris-Kyriakos Koliopoulos (ak.koliopoulos {[at]} gmail {[dot]} com)
+ */
+class wekaSparkUtils {
+
+  
+  def getNamesFromString(str:String):ArrayList[String]={
+    val list=new ArrayList[String]
+    val nm=str.split(",")
+    for(i<-0 to nm.length-1){list.add(nm(i))}
+    return list
+  }
+   
+  def convertDeserializedObjectToInstances(obj:Object):Instances={
+    return obj.asInstanceOf[Instances]
+  }
+  def convertDeserializedObjectToClassifier(obj:Object):Classifier={
+    return obj.asInstanceOf[Classifier]
+  }
+  def convertDeserializedObjectToCLusterer(obj:Object):Clusterer={
+    return obj.asInstanceOf[Clusterer]
+   }
+
+}
